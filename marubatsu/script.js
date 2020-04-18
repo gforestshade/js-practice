00c4d424579bfb53e0f849118ee92a3e775faf96
gforest_shade <shinapoka@gmail.com>
2020-04-18 11:35:15 +0900

    ×くん つよつよもーど

--- marubatsu/script.js
+++ marubatsu/script.js
@@ -1,134 +1,267 @@
-﻿
-var messageObj;
-var cellObjs;
-var cells;
-var gameOver;
-var playerTimeOutId;
+﻿'use strict';
 
+var cellObjs;
+var playerTimeOutId;
+var playerTimeOutSec;
+var messageObj;
+var board;
+
+class Board
+{
+  constructor(nTiles, nPlayer)
+  {
+    this.nTiles = nTiles;
+    this.nPlayer = nPlayer;
+  
+    this.cells = new Array(nTiles);
+    for (let i = 0; i < nTiles; i++)
+    {
+      this.cells[i] = new Array(nTiles);
+      for (let j = 0; j < nTiles; j++)
+      {
+        this.cells[i][j] = 0;
+      }
+    }
+  
+    this.winner = 0;
+    this.alreadyPut = 0;
+  }
+  
+  clone()
+  {
+    let newBoard = new Board(this.nTiles, this.nPlayer);
+    
+    newBoard.cells = new Array(this.nTiles);
+    for (let i = 0; i < this.nTiles; i++)
+    {
+      newBoard.cells[i] = new Array(this.nTiles);
+      for (let j = 0; j < this.nTiles; j++)
+      {
+        newBoard.cells[i][j] = this.cells[i][j];
+      }
+    }
+    
+    newBoard.winner = this.winner;
+    newBoard.alreadyPut = this.alreadyPut;
+    return newBoard;
+  }
+
+  nextPlayer(currentPlayer)
+  {
+    return currentPlayer % this.nPlayer + 1;
+  }
+
+  tryPut(row, column, p)
+  {
+    if (this.winner != 0) return false;
+    if (this.cells[row][column] != 0) return false;
+
+    this.cells[row][column] = p;
+    this.alreadyPut++;
+    return true;
+  }
+
+  AIPut(putMethod, p)
+  {
+    const cell = putMethod.call(this, p);
+    this.cells[cell[0]][cell[1]] = p;
+
+    this.alreadyPut++;
+    this.winner = this.judge();
+    refresh(this);
+  }
+  
+  getVacantList()
+  {
+    const N = this.nTiles;
+    let retList = [];
+    for (let i = 0; i < N; i++)
+    {
+      for (let j = 0; j < N; j++)
+      {
+        if (this.cells[i][j] == 0)
+          retList.push([i,j]);
+      }
+    }
+    return retList;
+  }
+
+  putRandom(p)
+  {
+    const vacantList = this.getVacantList();
+    return randomChoice(vacantList);
+  }
+  
+  putHard(p)
+  {
+    if (this.cells[1][1] == 0)
+    {
+      return [1,1];
+    }
+    else if (this.alreadyPut == 1)
+    {
+      return randomChoice([[0,0],[0,2],[2,0],[2,2]]);
+    }
+    else
+    {
+      const depth = this.nTiles * this.nTiles - this.alreadyPut;
+      const {te} = dfs(this, p, p, depth);
+      return randomChoice(te);
+    }
+  }
+
+  judge()
+  {
+    if (this.judge1(1)) return 1;
+    else if (this.judge1(2)) return 2;
+    else if (this.alreadyPut == this.nTiles * this.nTiles) return 12;
+    else return 0;
+  }
+
+  judge1(p)
+  {
+    const N = this.nTiles;
+    for (let i = 0; i < N; i++)
+    {
+      let rowComp = true;
+      for (let j = 0; j < N; j++)
+      {
+        rowComp = rowComp && (this.cells[i][j] == p);
+      }
+      if (rowComp) return true;
+    }
+
+    for (let j = 0; j < N; j++)
+    {
+      let colComp = true;
+      for (let i = 0; i < N; i++)
+      {
+        colComp = colComp && (this.cells[i][j] == p);
+      }
+      if (colComp) return true;
+    }
+
+    let ldComp = true;
+    let luComp = true;
+    for (let i = 0; i < N; i++)
+    {
+      ldComp = ldComp && (this.cells[i][i] == p);
+      luComp = luComp && (this.cells[i][N-1-i] == p);
+    }
+    if (ldComp || luComp) return true;
+  }
+}
+
+
+///////////////// gloval functions /////////////////////
+function randomChoice(a)
+{
+  const rnd = Math.floor(Math.random() * a.length);
+  return a[rnd];
+}
+
+
+function dfs(board, me, turnPlayer, leftDepth)
+{
+  const winner = board.judge();
+  if (winner == me)
+    return {score: 1000};
+  else if (winner > 0 && winner < 10)
+    return {score: -1000};
+  else if (leftDepth <= 0)
+    return {score: 0};
+
+
+  let m = {};
+  if (turnPlayer == me)
+    m.score = -Infinity;
+  else
+    m.score = Infinity;
+  
+  let vList = board.getVacantList();
+  let nextPlayer = board.nextPlayer(turnPlayer);
+  for (const v of vList)
+  {
+    let newBoard = board.clone();
+    newBoard.cells[v[0]][v[1]] = turnPlayer;
+    newBoard.alreadyPut++;
+    const mm = dfs(newBoard, me, nextPlayer, leftDepth - 1);
+
+    if ((turnPlayer == me && mm.score > m.score) || (turnPlayer != me && mm.score < m.score))
+    {
+      m.score = mm.score;
+      m.te = [v];
+    }
+    else if (mm.score == m.score)
+    {
+      m.te.push(v);
+    }
+  }
+  return m;
+}
 
 function onClick(row, column)
 {
-  if (winner != 0) return;
-  if (cells[row][column] != 0) return;
+  if (!board.tryPut(row, column, 1)) return;
+  board.winner = board.judge();
+  refresh(board);
 
-  cells[row][column] = 1;
-
-  judge();
-  refresh();
-
-  if (winner != 0) return;
+  if (board.winner != 0) return;
   
-  putRandom();
+  board.AIPut(board.putHard, 2);
   
   if (playerTimeOutId > 0)
     clearTimeout(playerTimeOutId);
 
-  playerTimeOutId = setTimeout(timeOut, 2000);
+  playerTimeOutId = setTimeout(timeOut, playerTimeOutSec * 1000);
 }
 
 function timeOut()
 {
-  if (winner == 0)
+  if (board.winner == 0)
   {
-    winner = 5;
-    refresh();
+    board.winner = 11;
+    refresh(board);
   }
 }
 
-function putRandom()
-{
-  let kouho = [];
-  for (let i = 0; i < 3; i++)
-  {
-    for (let j = 0; j < 3; j++)
-    {
-      if (cells[i][j] == 0)
-        kouho.push([i,j]);
-    }
-  }
-  let rnd = Math.floor(Math.random() * kouho.length);
-  cells[kouho[rnd][0]][kouho[rnd][1]] = 2;
-
-  judge();
-  refresh();
-}
-
-function judge()
-{
-  if (judge1(1)) winner = 1;
-  else if (judge1(2)) winner = 2;
-}
-
-function judge1(p)
+function refresh(board)
 {
   let N = 3;
   for (let i = 0; i < N; i++)
   {
-    let rowComp = true;
     for (let j = 0; j < N; j++)
     {
-      rowComp = rowComp && (cells[i][j] == p);
-    }
-    if (rowComp) return true;
-  }
-
-  for (let j = 0; j < N; j++)
-  {
-    let colComp = true;
-    for (let i = 0; i < N; i++)
-    {
-      colComp = colComp && (cells[i][j] == p);
-    }
-    if (colComp) return true;
-  }
-
-  let ldComp = true;
-  let luComp = true;
-  for (let i = 0; i < N; i++)
-  {
-    ldComp = ldComp && (cells[i][i] == p);
-    luComp = luComp && (cells[i][N-1-i] == p);
-  }
-  if (ldComp || luComp) return true;
-}
-
-function refresh()
-{
-  for (let i = 0; i < 3; i++)
-  {
-    for (let j = 0; j < 3; j++)
-    {
-      if (cells[i][j] == 0)
+      if (board.cells[i][j] == 0)
         cellObjs[i][j].innerText = '';
-      else if (cells[i][j] == 1)
+      else if (board.cells[i][j] == 1)
         cellObjs[i][j].innerText = '○';
-      else if (cells[i][j] == 2)
+      else if (board.cells[i][j] == 2)
         cellObjs[i][j].innerText = '×';
     }
   }
 
-  if (winner == 1)
+  if (board.winner == 1)
     messageObj.innerText = '○の勝ちー';
-  else if (winner == 2)
+  else if (board.winner == 2)
     messageObj.innerText = '×の勝ちー';
-  else if (winner == 5)
-    messageObj.innerText = '2秒経ったので△の勝ちー';
+  else if (board.winner == 11)
+    messageObj.innerText = `${playerTimeOutSec}秒経ったので△の勝ちー`;
+  else if (board.winner == 12)
+    messageObj.innerText = '漁夫の利を得た△の勝ちー';
+  // console.log(board.alreadyPut + "個置かれた");
 }
 
-function init(N)
+function init(nTiles, nPlayer, sec)
 {
-  cellObjs = new Array(N);
-  cells = new Array(N);
-
-  for (let i = 0; i < N; i++)
+  board = new Board(nTiles, nPlayer);
+  cellObjs = new Array(nTiles);
+  for (let i = 0; i < nTiles; i++)
   {
-    cellObjs[i] = new Array(N);
-    cells[i] = new Array(N);
-
-    for (let j = 0; j < N; j++)
+    cellObjs[i] = new Array(nTiles);
+    for (let j = 0; j < nTiles; j++)
     {
-      cells[i][j] = 0;
-      let cellId = "" + i + j;
+      let cellId = `${i}${j}`;
       cellObjs[i][j] = document.getElementById(cellId);
       cellObjs[i][j].onclick = (e) => onClick(i, j);
     }
@@ -136,8 +269,9 @@ function init(N)
 
   messageObj = document.getElementById('message');
   playerTimeOutId = -1;
-  winner = 0;
+  playerTimeOutSec = sec;
 }
 
 
-init(3);
+
+init(3, 2, 5.0);
